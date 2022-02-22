f69cc33edb376ebd59418493bf7a656765fe53fb
Elizabeth Adejumo <a.adejumo13@gmail.com>
2022-02-22 15:18:06 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = [];
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// bodyParser module needs to be set to use req.body
+app.use(bodyParser.urlencoded({extended: true}));
+
+// index page
+
+app.get("/", function(req, res) {
+
+  let today = new Date();
+
+  //formatting the date string
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  let day = today.toLocaleDateString("en-US", options);
+
+  // use res.render to load up an ejs view file
+  // rendering a file called list inside views folder with extension ejs and giving a value day to the letiable kindofDay
+  res.render("list", {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+
+  // adding each item to the list of items
+  items.push(item);
+
+  res.redirect("/");
+
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
